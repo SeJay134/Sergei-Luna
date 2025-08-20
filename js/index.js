0a554a4f458752bf81aafe1a79bbe335a80533ba
SeJay134 <patrushev.s.job@gmail.com>
2025-08-20 17:58:37 -0400

    added js

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const footer = document.querySelector(".container_footer");
+const div = document.createElement("div");
+div.classList.add("footer_box");
+div.textContent = "This is the footer";
+footer.appendChild(div);
