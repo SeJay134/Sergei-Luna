2a1b1c68aa1852e2728cf89d9e7bbea021afbfca
Sergei <patrushev.s.job@gmail.com>
2025-08-20 20:27:43 -0400

    added js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 const footer = document.querySelector(".container_footer");
 const div = document.createElement("div");
 div.classList.add("footer_box");
-div.textContent = "This is the footer";
+div.textContent = "div";
 footer.appendChild(div);
+
+const today = new Date().getFullYear();
+const copyright = "Sergei Patrushev";
+
+const find_footer_box = document.querySelector(".footer_box");
+const footer_box_p = document.createElement("p");
+footer_box_p.classList.add("footer_box_p");
+footer_box_p.textContent = copyright + " " + today + " " + "©";
+find_footer_box.appendChild(footer_box_p);
