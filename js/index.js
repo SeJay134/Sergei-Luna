5dcf2732b71bab26dba0175e58a5a08d7f407a7e
SeJay134 <patrushev.s.job@gmail.com>
2025-08-21 17:58:42 -0400

    added in js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const footer = document.querySelector(".container_footer");
 const div = document.createElement("div");
 div.classList.add("footer_box");
-div.textContent = "div";
+
 footer.appendChild(div);
 
 const today = new Date().getFullYear();
@@ -10,5 +10,19 @@ const copyright = "Sergei Patrushev";
 const find_footer_box = document.querySelector(".footer_box");
 const footer_box_p = document.createElement("p");
 footer_box_p.classList.add("footer_box_p");
-footer_box_p.textContent = copyright + " " + today + " " + "©";
+footer_box_p.textContent = copyright + " " + "©" + " " + today;
 find_footer_box.appendChild(footer_box_p);
+
+let skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"];
+const skillsSection = document.getElementById("Skills");
+let skillsList = skillsSection.querySelector("ul");
+
+for (let i = 0; i < skills.length; i++) {
+    if () {
+        let skill = document.createComment("ul");
+        skill[0].innerHTML;
+        skillsList.appendChild(li);
+    }
+   
+
+}
