cde0a6f3599026f07e2f0439a02359b511913dc9
SeJay134 <patrushev.s.job@gmail.com>
2025-08-22 17:57:46 -0400

    added css js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
+/* footer div */
 const footer = document.querySelector(".container_footer");
 const div = document.createElement("div");
 div.classList.add("footer_box");
-
 footer.appendChild(div);
 
+/* date */
 const today = new Date().getFullYear();
+/* name */
 const copyright = "Sergei Patrushev";
 
+/* copyright */
 const find_footer_box = document.querySelector(".footer_box");
 const footer_box_p = document.createElement("p");
 footer_box_p.classList.add("footer_box_p");
@@ -22,15 +25,13 @@ const skillsSection = document.getElementById("Skills");
 /* Create a variable named skillsList and use "DOM Selection" to query the skillsSection (instead of the entire document) to select the <ul> element you created earlier in this assignment */
 let skillsList = skillsSection.querySelector("ul");
 
+
 /* Create a for loop to iterate over your skills Array */
 for (let i = 0; i < skills.length; i++) {
         /* Inside the loop, create a variable named skill to create a new list item (li) element */
         let skill = document.createElement("li");
         /* Still inside the loop, use the skill variable to set the inner text to the current Array element */
-        skill[i].innerHTML;
+        skill.innerHTML = skills[i];
         /* The last thing we'll do inside the loop is append the skill element to the skillsList element */
         skillsList.appendChild(skill);
-    
-   
-
 }
